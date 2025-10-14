959271bd505a45467698593f3f4b8697700188c7
Baxtiyor <baxtiyorr545@gmail.com>
2025-10-14 11:11:51 +0500

    DatabaseRoleMapper Created

--- src/main/java/uz/pdp/dbcontrol/model/entity/DatabaseRole.java
+++ src/main/java/uz/pdp/dbcontrol/model/entity/DatabaseRole.java
@@ -1,19 +1,25 @@
 package uz.pdp.dbcontrol.model.entity;
 
-import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 import uz.pdp.dbcontrol.model.base.BaseEntity;
 
-@Entity
+@Document(collection = "database_roles")
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 public class DatabaseRole extends BaseEntity {
+    @Field("name")
     private String name;
+    
+    @Field("code")
     private String code;
+    
+    @Field("description")
     private String description;
 }
